d973ab38f29ae8a49d4f8e138073f700ddb8ba39
deokbin <deokbin@naver.com>
2025-02-27 17:21:35 +0900

    feat: 댓글 기능 추가

--- js/index.js
+++ js/index.js
@@ -1,19 +1,43 @@
-console.log(
-  "%c" +
-    " __      __  ______   __  __   ______     " +
-    "\n" +
-    "/\\ \\  __/\\ \\ /\\__  _\\ /\\ \\ /\\ \\ /\\__  _\\    " +
-    "\n" +
-    "\\ \\ \\/\\ \\ \\ \\/_\\/\\ \\/ \\ \\ \\/'/'\\/ _/\\ \\/    " +
-    "\n" +
-    " \\ \\ \\ \\ \\ \\ \\ \\ \\ \\  \\ \\ , <    \\ \\ \\    " +
-    "\n" +
-    "  \\ \\ \\_/ \\_\\ \\ \\_\\ \\__\\ \\ \\\\`\\   \\_\\ \\__ " +
-    "\n" +
-    "   \\ `\\___x___/ /\\_____\\\\ \\_\\ \\_\\ /\\_____\\ " +
-    "\n" +
-    "    '/__//__/  /_____/ \\/_/\\/_/ /_____/",
-  "color: #d81b60; font-size: 16px; font-weight: bold;"
-);
+function displayComments() {
+  const commentList = document.getElementById("comment-list");
+  const comments = JSON.parse(localStorage.getItem("comments")) || [];
 
-console.log("알맞은 스크립트를 작성하세요");
+  commentList.innerHTML = "";
+  comments.forEach((comment) => {
+    const li = document.createElement("li");
+    li.classList.add("comment-item");
+    li.innerHTML = `
+      <div class="comment-author">
+        <img src="./images/comment-author-icon.png" alt="사용자 프로필 이미지" />
+        <span>방문자</span>
+      </div>
+      <div class="comment-content">
+        ${comment}
+      </div>
+    `;
+    commentList.appendChild(li);
+  });
+}
+
+function saveComment() {
+  const commentInput = document.getElementById("commentInput");
+  const commentText = commentInput.value.trim();
+
+  if (commentText) {
+    const comments = JSON.parse(localStorage.getItem("comments")) || [];
+    comments.push(commentText);
+
+    localStorage.setItem("comments", JSON.stringify(comments));
+    displayComments();
+
+    commentInput.value = "";
+    alert("댓글이 등록되었습니다!");
+  }
+}
+
+document.addEventListener("DOMContentLoaded", displayComments);
+document.getElementById("submitComment").addEventListener("click", saveComment);
+
+document.getElementById("resetComment").addEventListener("click", () => {
+  document.getElementById("commentInput").value = "";
+});
